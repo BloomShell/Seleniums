3686b41211145ead0f0369fecdf55e8f208500be
BloomShell <77837516+BloomShell@users.noreply.github.com>
2022-10-02 21:51:39 +0200

    Add files via upload

--- yahoo-finance/symbols.py
+++ yahoo-finance/symbols.py
@@ -1,27 +1,76 @@
 from dotenv import load_dotenv
 from base import Webdriver
+import pandas as pd
 load_dotenv()
+import math
+import json
+import time
+import re
 import os
 
 
 if __name__ == "__main__":
 
-    wd = Webdriver(driver_path="../chromedriver.exe", headless=False)
-    wd.goto("https://finance.yahoo.com/screener/new")
-    wd.click(os.getenv("ACCEPT_PRIVACY_BUTTON"))
+    xpaths = json.load(open("xpaths.json"))
 
-    # Remove all the default filters...
-    for _ in range(4):
-        wd.click(os.getenv("REMOVE_FIRST_FILTER_BUTTON"))
+    for exchange in xpaths:
 
-    # Open the Filters Window...
-    wd.click(os.getenv("ADD_FILTER_BUTTON"))
+        t0 = time.time()
+        print(f"Starting... Exchange: {exchange}")
 
-    # Add 'Exchange' as a filter...
-    wd.click(os.getenv("ADD_EXCHANGE_FILTER_BUTTON"))
+        wd = Webdriver(driver_path="../chromedriver.exe", headless=False)
+        wd.goto("https://finance.yahoo.com/screener/new")
+        wd.click(os.getenv("ACCEPT_PRIVACY_BUTTON"))
 
-    # Close the Filters Window...
-    wd.click(os.getenv("CLOSE_FILTERS_BUTTON"))
+        # Remove all the default filters...
+        for _ in range(4):
+            wd.click(os.getenv("REMOVE_FIRST_FILTER_BUTTON"))
 
-    # Open the Exchange filter window...
-    wd.click(os.getenv("ADD_EXCHANGES_BUTTON"))
+        # Open the Filters Window...
+        wd.click(os.getenv("ADD_FILTER_BUTTON"))
+
+        # Add 'Exchange' as a filter...
+        wd.click(os.getenv("ADD_EXCHANGE_FILTER_BUTTON"))
+
+        # Close the Filters Window...
+        wd.click(os.getenv("CLOSE_FILTERS_BUTTON"))
+
+        # Open the Exchange filter window...
+        wd.click(os.getenv("ADD_EXCHANGES_BUTTON"))
+
+        for xpath in xpaths[exchange]:
+            wd.click(xpath)
+
+        wd.click(os.getenv("FIND_STOCKS_BUTTON"))
+        wd.click(os.getenv("OPEN_NUMBER_OF_RECORDS_WINDOW_BUTTON"))
+        wd.click(os.getenv("SELECT_SHOW_100_RECORDS_BUTTON"))
+
+        # Inferred number of pages to search based
+        # on screener message.
+        n_found = int(re.compile("\d+").findall(re.compile("of\s\d+\sresults") \
+            .findall(wd.get().find("span", {"class": "Mstart(15px) Fw(500) Fz(s)"})
+            .getText())[0])[0])
+        n_pages = math.ceil(n_found / 100)
+
+        # Iterate through (n_pages - 1)...
+        results = []
+        for i in range(n_pages - 1):
+            table = wd.get_table("div", {"id": "fin-scr-res-table"},
+                                 exclude=["span"], min_rows=100)
+            wd.click(os.getenv("NEXT_PAGE_BUTTON"))
+            results.append(table)
+            time.sleep(10)
+
+        # Last page, observations <= 100, min_rows = 1.
+        table = wd.get_table("div", {"id": "fin-scr-res-table"},
+                             exclude=["span"], min_rows=1)
+        results.append(table)
+        t1 = time.time()
+        duration = t1 - t0
+        print(f"Terminated! time: {round(duration, 2)} second(s).")
+
+        # Save to Json...
+        data = pd.concat(results).drop_duplicates('Symbol').set_index('Symbol')
+        with open(f"output/{exchange}.json", "w") as f:
+            json.dump(data, f, indent=2)
+        time.sleep(5)
