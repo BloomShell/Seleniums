29cbf76a4fb12bc39b48245418f0bf095715bd5f
BloomShell <77837516+BloomShell@users.noreply.github.com>
2022-10-02 21:53:58 +0200

    Update base.py

--- base.py
+++ base.py
@@ -2,6 +2,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+import pandas as pd
 import selenium
 import time
 import bs4
