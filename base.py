5ce5069aef8ccb44b52e884d1bfcbec59420309d
BloomShell <77837516+BloomShell@users.noreply.github.com>
2022-10-05 09:15:49 +0200

    Update base.py

--- base.py
+++ base.py
@@ -16,6 +16,10 @@ class Webdriver(object):
             headless: bool
     ):
         options = Options()
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option('useAutomationExtension', False)
+        options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
         if headless:
             options.headless = True
         prefs = {
